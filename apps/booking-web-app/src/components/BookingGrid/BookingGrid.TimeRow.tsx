c56f63e8d239cf093a17b56ac23d44d977ee147b
Adán <adan.gonzalez.lopez@gmail.com>
2025-04-13 01:00:21 +0200

    feat: booked slot style

--- apps/booking-web-app/src/components/BookingGrid/BookingGrid.TimeRow.tsx
+++ apps/booking-web-app/src/components/BookingGrid/BookingGrid.TimeRow.tsx
@@ -10,14 +10,14 @@ interface TimeRowProps {
 
 export default function TimeRow({ row, isLast }: TimeRowProps) {
   return (
-    <div className="grid grid-cols-[100px_repeat(auto-fit,minmax(100px,1fr))] border-b border-gray-200 h-[30px]">
-      <div className={`bg-pink-400 text-black font-medium text-sm flex items-center justify-center px-1 py-0.5 border-r border-gray-200 ${isLast ? 'rounded-bl-lg' : 'border-r'}`}>
+    <div className="grid grid-cols-[100px_repeat(auto-fit,minmax(100px,1fr))] border-b border-gray-300 h-[30px]">
+      <div className={`bg-pink-400 text-black font-medium text-sm flex items-center justify-center px-1 py-0.5 border-r border-gray-300 ${isLast ? 'rounded-bl-lg' : 'border-r'}`}>
         {row.time}
       </div>
       {row.cells.map((cell, index) => (
         <div
           key={index}
-          className={`px-1 py-0.5 text-center font-bold ${cell.occupied ? 'bg-red-200' : 'bg-white'} ${index === row.cells.length - 1 && isLast ? 'rounded-br-lg' : 'border-r'} border-r border-gray-200`}
+          className={`px-1 py-0.5 text-center font-bold ${cell.occupied ? 'bg-gray-200' : 'bg-white'} ${index === row.cells.length - 1 && isLast ? 'rounded-br-lg' : 'border-r'} border-r border-gray-300`}
           title={cell.label}
         >
           {cell.label}
